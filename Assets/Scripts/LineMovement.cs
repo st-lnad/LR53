e9806f9a39920c97e1b49641497d12131dbb7801
st-lnad <lev.potanin@yandex.ru>
2023-04-30 16:51:10 +0300

    Add bezier movement

--- Assets/Scripts/LineMovement.cs
+++ Assets/Scripts/LineMovement.cs
@@ -2,49 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineMovement : MonoBehaviour
-{
-    protected Vector3 from;
-    protected Vector3 to;
-    //Vector3 increment;
-    protected float movement_time;
-    protected float elapsedTime = 0.0f;
-    protected bool is_moving = false;
-    
+public class LineMovement : Movement
+{   
     
     public void init(Vector3 from, Vector3 to, float time)
     {
-        if (!is_moving) {
-            is_moving = true;
             this.from = from;
             this.to = to;
             movement_time = time;
-            }
-        else
-        {
-            Debug.Log("Object is moving. Fuck you");
-        }
+            is_movement_allowed = true;
+    }
+    public new void cancel()
+    {
+        var tmp = to;
+        to = from;
+        from = tmp;
+        elapsedTime = movement_time - elapsedTime;
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        is_movement_allowed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        if (gameObject.transform.position != to)
-        {
-            var a = Time.deltaTime;
-            gameObject.transform.position =  Vector3.Lerp(from, to, elapsedTime/movement_time);
-        }
-        else
-        {
-            elapsedTime = 0f;
-            is_moving = false;
+        if (is_movement_allowed) { 
+            elapsedTime += Time.deltaTime;
+            if (gameObject.transform.position != to)
+            {
+                gameObject.transform.position =  Vector3.Lerp(from, to, elapsedTime/movement_time);
+            }
+            else
+            {
+                elapsedTime = 0f;
+                is_movement_allowed = false;
+            }
         }
     }
 }
--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -2,36 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Road : MyEntity
+public class Road : MonoBehaviour
 {
     public float move_time = 5;
-    public GameObject[] villages = new GameObject[2];
+    public GameObject[] ends = new GameObject[2];
+    public Transform[] points = new Transform[4];
 
-    public void init(GameObject[] villages)
+
+
+    public GameObject[] get_ends()
     {
-        this.villages[0] = villages[0];
-        this.villages[1] = villages[1];
-
-        // Добавляем компонент Line Renderer к объекту дороги
-        gameObject.AddComponent<LineRenderer>();
-
-        // Устанавливаем ширину линии
-        gameObject.GetComponent<LineRenderer>().startWidth = 0.2f;
-        gameObject.GetComponent<LineRenderer>().endWidth = 0.2f;
-
-        // Устанавливаем точки линии дороги
-        gameObject.GetComponent<LineRenderer>().positionCount = 2;
-        gameObject.GetComponent<LineRenderer>().SetPosition(0, villages[0].transform.position);
-        gameObject.GetComponent<LineRenderer>().SetPosition(1, villages[1].transform.position);
-    }
-    public GameObject[] get_villages()
-    {
-        return villages;
+        return ends;
     }
     public int get_direction(Vector3 current_position)
     {
 
-        return ((villages[0].transform.position == current_position) ? 1 : -1); 
+        return ((ends[0].transform.position == current_position) ? 1 : -1); 
     }
     
 }
